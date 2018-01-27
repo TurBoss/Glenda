a090062bb57e9573c7d9c559c0560b1aced5ceec
TurBoss <j.l.toledano.l@gmail.com>
2018-01-28 00:40:02 +0100

    daemonize Glenda

--- main.py
+++ main.py
@@ -14,6 +14,7 @@
 import sys
 import logging
 import yaml
+import argparse
 
 from matrix_client.client import MatrixClient
 from matrix_client.api import MatrixRequestError
@@ -21,86 +22,124 @@ from requests.exceptions import MissingSchema
 
 from ircbot import IrcBot
 
+from daemon_python import DaemonPython
+
+
+class Glendaemon(DaemonPython):
+
+    def __init__(self, pid_file):
+        super(DaemonPython, self).__init__()
+
+        self.pid_file = pid_file
+
+        with open("config.yaml", 'r') as yml_file:
+            self.cfg = yaml.load(yml_file)
+
+        # Matrix
+
+        self.rooms = {}
+
+        self.bot_owner = self.cfg["bot_owner"]
+
+        self.host = self.cfg["matrix"]["host"]
+        self.domain = self.cfg["matrix"]["domain"]
+
+        self.username = self.cfg["matrix"]["username"]
+        self.password = self.cfg["matrix"]["password"]
+
+        # IRC
+
+        self.irc_server = self.cfg["irc"]["spring_server"]
+
+        self.bot_nick = self.cfg["irc"]["bot_nick"]
+        self.bot_password = self.cfg["irc"]["bot_password"]
+
+        self.rooms_id = {}
+        self.channels = []
+
+        self.client = MatrixClient(self.host)
+
+        self.bot = IrcBot(self.channels,
+                          self.domain,
+                          self.username,
+                          self.bot_nick,
+                          self.irc_server,
+                          self.bot_password,
+                          self.client,
+                          self.rooms,
+                          self.rooms_id,
+                          self.bot_owner)
+
+    def run(self):
+
+        print("Bridged rooms:")
+
+        for channel, room in self.cfg["channels"].items():
+            print("{0} <-> {1}".format(channel, room[0]))
+
+            self.rooms_id[channel] = room
+
+            self.channels.append(channel)
+
+        try:
+            self.client.login_with_password(self.username, self.password)
+        except MatrixRequestError as e:
+            print(e)
+            if e.code == 403:
+                print("Bad username or password.")
+                sys.exit(4)
+            else:
+                print("Check your sever details are correct.")
+                sys.exit(2)
+        except MissingSchema as e:
+            print("Bad URL format.")
+            print(e)
+            sys.exit(3)
+
+        try:
+            for k, v in self.rooms_id.items():
+                self.rooms[k] = self.client.join_room(v[0])
+        except MatrixRequestError as e:
+            print(e)
+            if e.code == 400:
+                print("Room ID/Alias in the wrong format")
+                sys.exit(11)
+            else:
+                print("Couldn't find room.")
+                sys.exit(12)
+
+        self.bot.start()
+
 
 def main():
-    with open("config.yaml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
 
-    # Matrix
+    parser = argparse.ArgumentParser(description='Glenda Service.')
 
-    rooms = {}
+    parser.add_argument('operation',
+                        metavar='OPERATION',
+                        type=str,
+                        help='Operation with daemon. Accepts any of these values: start, stop, restart, status',
+                        choices=['start', 'stop', 'restart', 'status'])
 
-    bot_owner = cfg["bot_owner"]
+    args = parser.parse_args()
+    operation = args.operation
 
-    host = cfg["matrix"]["host"]
-    domain = cfg["matrix"]["domain"]
+    pidfile = '/var/run/mydaemon.pid'
+    daemon = Glendaemon(pidfile)
 
-    username = cfg["matrix"]["username"]
-    password = cfg["matrix"]["password"]
-
-    # IRC
-
-    irc_server = cfg["irc"]["spring_server"]
-
-    bot_nick = cfg["irc"]["bot_nick"]
-    bot_password = cfg["irc"]["bot_password"]
-
-    rooms_id = {}
-    channels = []
-
-    print("Bridged rooms:")
-
-    for channel, room in cfg["channels"].items():
-        print("{0} <-> {1}".format(channel, room[0]))
-
-        rooms_id[channel] = room
-
-        channels.append(channel)
-
-        # print(msg)
-    client = MatrixClient(host)
-
-    try:
-        client.login_with_password(username, password)
-    except MatrixRequestError as e:
-        print(e)
-        if e.code == 403:
-            print("Bad username or password.")
-            sys.exit(4)
+    if operation:
+        if operation == 'start':
+            daemon.start()
+        if operation == 'restart':
+            daemon.restart()
+        if operation == 'stop':
+            daemon.stop()
         else:
-            print("Check your sever details are correct.")
+            print("Unknown command")
             sys.exit(2)
-    except MissingSchema as e:
-        print("Bad URL format.")
-        print(e)
-        sys.exit(3)
 
-    try:
-        for k, v in rooms_id.items():
-            rooms[k] = client.join_room(v[0])
-    except MatrixRequestError as e:
-        print(e)
-        if e.code == 400:
-            print("Room ID/Alias in the wrong format")
-            sys.exit(11)
-        else:
-            print("Couldn't find room.")
-            sys.exit(12)
-
-    bot = IrcBot(channels,
-                 domain,
-                 username,
-                 bot_nick,
-                 irc_server,
-                 bot_password,
-                 client,
-                 rooms,
-                 rooms_id,
-                 bot_owner)
-    bot.start()
+        sys.exit(0)
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.WARNING)
-
+if __name__ == "__main__":
     main()
