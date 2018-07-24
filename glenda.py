957d2db56fb027c8d753544bd2520739529cf118
abma <spring@abma.de>
2018-07-24 23:31:36 +0200

    - auto detect room id - don't iterate always over all configured rooms - refactor for less idents / smaller functions - increase log level on error - force python3

--- glenda.py
+++ glenda.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import logging
@@ -30,55 +30,58 @@ class Glenda:
 
         self.cfg = cfg
 
-        self.rooms = []
-        self.client_rooms = {}
+        self.matrix_rooms = {}
+        self.lobby_rooms = {}
         self.matrix_client = None
         self.lobby_client = None
 
     # Called when a message is recieved from the matrix
     def on_room_message(self, room, event):
         if event['sender'] != "@{}:{}".format(self.cfg["matrix"]["username"], self.cfg["matrix"]["domain"]):
-            if event['type'] == "m.room.message":
-                if event['content']['msgtype'] == "m.text":
+           # ignore messages to others
+           self.log.error("message to other: %s" %(event))
+           return
+        if event['type'] != "m.room.message":
+           self.log.error("unknown event: %s" %(event))
+           return
+        if not event["room_id"] in self.matrix_rooms:
+           self.log.error("Unknown room: %s vs %s" %(event, self.matrix_rooms))
+           return
 
-                    user = self.matrix_client.get_user(event['sender'])
-                    user_display_name = user.get_display_name()
+        lobby_room = self.matrix_rooms[event["room_id"]]
+        if event['content']['msgtype'] == "m.text":
+            user = self.matrix_client.get_user(event['sender'])
+            user_display_name = user.get_display_name()
+            self.lobby_client.say(lobby_room, "<{}> {}".format(user_display_name, event['content']['body']))
+        elif event['content']['msgtype'] == "m.emote":
+            user = self.matrix_client.get_user(event['sender'])
+            user_display_name = user.get_display_name()
+            self.lobby_client.say_ex(lobby_room, "<{}> {}".format(user_display_name, event['content']['body']))
+        elif event['content']['msgtype'] == "m.image":
+            user = self.matrix_client.get_user(event['sender'])
+            user_display_name = user.get_display_name()
+            mxc_url = event['content']['url']
+            o = urlparse(mxc_url)
+            domain = o.netloc
+            pic_code = o.path
+            url = "https://{0}/_matrix/media/v1/download/{0}{1}".format(domain, pic_code)
+            msg = "<{}> {}".format(user_display_name, url)
+            self.lobby_client.say_ex(lobby_room, "<{}> {}".format(user_display_name, msg))
 
-                    for lobby_room, matrix_room in self.rooms:
-                        if event["room_id"] == matrix_room[1]:
-                            self.lobby_client.say(lobby_room, "<{}> {}".format(user_display_name,
-                                                                               event['content']['body']))
-                elif event['content']['msgtype'] == "m.emote":
-
-                    user = self.matrix_client.get_user(event['sender'])
-                    user_display_name = user.get_display_name()
-
-                    for lobby_room, matrix_room in self.rooms:
-                        if event["room_id"] == matrix_room[1]:
-                            self.lobby_client.say_ex(lobby_room, "<{}> {}".format(user_display_name,
-                                                                               event['content']['body']))
-                elif event['content']['msgtype'] == "m.image":
-
-                    user = self.matrix_client.get_user(event['sender'])
-                    user_display_name = user.get_display_name()
-
-                    for lobby_room, matrix_room in self.rooms:
-                        if event["room_id"] == matrix_room[1]:
-                            mxc_url = event['content']['url']
-                            o = urlparse(mxc_url)
-
-                            domain = o.netloc
-                            pic_code = o.path
-
-                            url = "https://{0}/_matrix/media/v1/download/{0}{1}".format(domain, pic_code)
-
-                            msg = "<{}> {}".format(user_display_name, url)
-
-                            self.lobby_client.say_ex(lobby_room, "<{}> {}".format(user_display_name, msg))
-                else:
-                    print(event)
+    def setup_bridge(self, lobby_room, matrix_room):
+        self.lobby_client.channels_to_join.append("#{}".format(lobby_room))
+        try:
+            response = self.matrix_client.join_room(matrix_room)
+            response.add_listener(self.on_room_message)
+            self.lobby_rooms[lobby_room] = response
+            self.matrix_rooms[response.room_id] = lobby_room
+        except MatrixRequestError as e:
+            if e.code == 400:
+                self.log.error("Room ID/Alias in the wrong format")
+                sys.exit(11)
             else:
-                print(event)
+                self.log.error("Couldn't find  %s room." %(matrix_room))
+                sys.exit(12)
 
     async def run(self):
 
@@ -92,34 +95,19 @@ class Glenda:
         except MatrixRequestError as e:
             self.log.debug(e)
             if e.code == 403:
-                self.log.debug("Bad username or password.")
+                self.log.error("Bad username or password.")
                 sys.exit(4)
             else:
-                self.log.debug("Check your sever details are correct.")
+                self.log.error("Check your sever details are correct.")
                 sys.exit(2)
 
         except MissingSchema as e:
-            self.log.debug("Bad URL format.")
-            self.log.debug(e)
+            self.log.error("Bad URL format.")
+            self.log.error(e)
             sys.exit(3)
 
         for lobby_room, matrix_room in self.cfg["rooms"].items():
-
-            self.rooms.append((lobby_room, matrix_room))
-
-            self.lobby_client.channels_to_join.append("#{}".format(lobby_room))
-
-            try:
-                self.client_rooms[lobby_room] = self.matrix_client.join_room(matrix_room[0])
-                self.client_rooms[lobby_room].add_listener(self.on_room_message)
-
-            except MatrixRequestError as e:
-                if e.code == 400:
-                    self.log.debug("Room ID/Alias in the wrong format")
-                    sys.exit(11)
-                else:
-                    self.log.debug("Couldn't find room.")
-                    sys.exit(12)
+            self.setup_bridge(lobby_room, matrix_room)
 
         self.lobby_client.login(self.cfg["lobby"]["username"], self.cfg["lobby"]["pwd"])
 
@@ -128,15 +116,15 @@ class Glenda:
 
         except MatrixRequestError as e:
             if e.code == 403:
-                self.log.debug("Bad username or password.")
+                self.log.error("Bad username or password.")
                 sys.exit(4)
             else:
-                self.log.debug("Check your sever details are correct.")
+                self.log.error("Check your sever details are correct.")
                 sys.exit(2)
 
         except MissingSchema as e:
-            self.log.debug("Bad URL format.")
-            self.log.debug(e)
+            self.log.error("Bad URL format.")
+            self.log.error(e)
             sys.exit(3)
 
         self.matrix_client.start_listener_thread()
@@ -153,15 +141,18 @@ def main():
 
     @glenda.lobby_client.on("said")
     def on_lobby_said(parsed, user, target, text):
-        if user != cfg["lobby"]["username"]:
-            matrix_room = glenda.client_rooms[target]
-            matrix_room.send_text("<{}> {}".format(user, text))
+        if user == cfg["lobby"]["username"]:
+            return
+        print(glenda.lobby_rooms)
+        matrix_room = glenda.lobby_rooms[target]
+        matrix_room.send_text("<{}> {}".format(user, text))
 
     @glenda.lobby_client.on("saidex")
     def on_lobby_saidex(parsed, user, target, text):
-        if user != cfg["lobby"]["username"]:
-            matrix_room = glenda.client_rooms[target]
-            matrix_room.send_emote("<{}> {}".format(user, text))
+        if user == cfg["lobby"]["username"]:
+            return
+        matrix_room = glenda.lobby_rooms[target]
+        matrix_room.send_emote("<{}> {}".format(user, text))
 
     loop.run_forever()
 
